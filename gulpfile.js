a24524e293c8fb9ebfcba93976966b1593db0cb3
Marcin Szamotulski <mszamot@gmail.com>
2016-06-06 11:36:56 +0100

    add MIT license

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const babel = require("gulp-babel"),
   gulp = require("gulp"),
   karma = require("karma"),
+  license = require("gulp-license"),
   rename = require("gulp-rename"),
   uglify = require("gulp-uglify");
 
@@ -9,9 +10,15 @@ gulp.task('build', () => {
     .pipe(babel({
       presets: ["es2015"]
     }))
+    .pipe(license("MIT", {
+      organization: "Rubble LTD. All rights reserved."
+    }))
     .pipe(gulp.dest('dist'))
     .pipe(rename("ref.min.js"))
     .pipe(uglify())
+    .pipe(license("MIT", {
+      organization: "Rubble LTD. All rights reserved.",
+    }))
     .pipe(gulp.dest('dist'));
 });
 
